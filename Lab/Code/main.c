91b214db9a6bb363bd58aafbbc605b52135486da
hantx <txhan.nju@gmail.com>
2020-10-14 22:06:03 +0800

    this a small test for a calculater support float

--- Lab/Code/main.c
+++ Lab/Code/main.c
@@ -2,14 +2,18 @@
 
 extern FILE* yyin;
 extern int yylex (void);
+extern void yyrestart ( FILE *input_file  );
+extern int yyparse();
 
 int main(int argc, char** argv) {
-    if (argc > 1) {
-        if (!(yyin = fopen(argv[1], "r"))) {
-            perror(argv[1]);
-            return 1;
-        }
+    if(argc <= 1)   return 1;
+    FILE* f =fopen(argv[1], "r");
+    if(!f)
+    {
+        perror(argv[1]);
+        return 1;
     }
-    while (yylex() != 0);
+    yyrestart(f);
+    yyparse();
     return 0;
 }
